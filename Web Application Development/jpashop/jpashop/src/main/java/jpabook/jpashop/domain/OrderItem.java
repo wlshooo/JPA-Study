40f65571b884bf579d3f79a4961bc8efa7fec75c
jinhoLee <kswlsgh@naver.com>
2023-05-19 00:01:17 +0900

    주문 기능 테스트

--- Web Application Development/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ Web Application Development/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,13 +1,17 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -24,4 +28,26 @@ public class OrderItem {
 
     private int orderPrice; //주문가격
     private int count; //주문 수량
+
+
+
+    //생성 메서드
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+    //비즈니스 로직
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //조회 로직
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
