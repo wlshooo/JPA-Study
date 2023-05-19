c4c377517c2751a1558add9ae0fb3d050ddce25d
jinhoLee <kswlsgh@naver.com>
2023-05-20 00:00:11 +0900

    end

--- Web Application Development/jpashop/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ Web Application Development/jpashop/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,22 +1,39 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import java.util.List;
 
+@Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ItemService {
     private final ItemRepository itemRepository;
 
+    @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
     }
+
+    @Transactional
+    public void updateItem(Long id, String name, int price, int stockQuantity)
+    {
+        Item item = itemRepository.findOne(id);
+        item.setName(name);
+        item.setPrice(price);
+        item.setStockQuantity(stockQuantity);
+    }
+
     public List<Item>findItems() {
         return itemRepository.findAll();
     }
@@ -24,4 +41,6 @@ public class ItemService {
     public Item findOne(Long itemId) {
         return itemRepository.findOne(itemId);
     }
+
+
 }
