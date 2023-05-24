760d2d76ab6c4ec9554072a59787000cb77e6415
jinhoLee <kswlsgh@naver.com>
2023-05-24 20:07:08 +0900

    section1 clear

--- API development and performance optimization/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ API development and performance optimization/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,10 +15,13 @@ public class Member {
     @Id @GeneratedValue
     @Column(name = "member_id")
     private Long id;
+
+    @NotEmpty
     private String name;
 
     @Embedded
     private Address address;
+
 
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
