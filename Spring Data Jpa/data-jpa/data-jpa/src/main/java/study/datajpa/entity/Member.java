437f4bbf285c2aab8e1a8c033f527863f3f3a460
jinhoLee <kswlsgh@naver.com>
2023-05-31 02:47:44 +0900

    section2 예제 도메인 모델

--- Spring Data Jpa/data-jpa/data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ Spring Data Jpa/data-jpa/data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,27 +1,40 @@
 package study.datajpa.entity;
 
-import lombok.Generated;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username","age"})
 public class Member {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    protected Member() {
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) { //연관관계
+        this.team = team;
+        team.getMember().add(this);
     }
 }
