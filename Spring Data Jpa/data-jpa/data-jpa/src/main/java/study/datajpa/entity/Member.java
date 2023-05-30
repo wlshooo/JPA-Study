4f73bdb6ab23d0b6dc46087f61e19ebed1fad30d
jinhoLee <kswlsgh@naver.com>
2023-05-30 22:40:38 +0900

    section2 setting

---
+++ Spring Data Jpa/data-jpa/data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,27 @@
+package study.datajpa.entity;
+
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+
+    protected Member() {
+    }
+
+    public Member(String username) {
+        this.username = username;
+    }
+}
