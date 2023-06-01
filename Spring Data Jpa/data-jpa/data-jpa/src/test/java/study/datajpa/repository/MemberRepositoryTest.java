2e85c5320bf1ffea39452bba766a3c8779715348
jinhoLee <kswlsgh@naver.com>
2023-06-02 01:32:45 +0900

    section4 @EntityGraph

--- Spring Data Jpa/data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ Spring Data Jpa/data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -216,4 +216,26 @@ class MemberRepositoryTest {
         System.out.println("member5 = " + member5);
         assertThat(resultCount).isEqualTo(3);
     }
+
+    @Test
+    public void findMemberLazy() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+        Member member1 = new Member("member1",10,teamA);
+        Member member2 = new Member("member2",10,teamB);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        List<Member> members = memberRepository.findEntityGraphByUsername("member1");
+        for (Member member : members) {
+            System.out.println("member.getUsername() = " + member.getUsername());
+            System.out.println("member.getTeam().getClass() = " + member.getTeam().getClass());
+            System.out.println("member.team = " +  member.getTeam().getMember());
+        }
+    }
 }
