3fa4dd4539c971555da4d394216ac1e07691c631
jinhoLee <kswlsgh@naver.com>
2023-06-02 18:41:41 +0900

    section5 Auditing

--- Spring Data Jpa/data-jpa/data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
+++ Spring Data Jpa/data-jpa/data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
@@ -1,9 +1,13 @@
 package study.datajpa.entity;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.repository.MemberRepository;
+import study.datajpa.repository.MemberRepositoryCustom;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -18,6 +22,12 @@ class MemberTest {
 
     @PersistenceContext
     EntityManager em;
+    @Autowired
+    @Qualifier("memberRepository")
+    private MemberRepositoryCustom memberRepositoryCustom;
+
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Test
     public void testEntity() {
@@ -49,6 +59,24 @@ class MemberTest {
             System.out.println("->member.team = " + member.getTeam());
         }
         
+    }
+
+    @Test
+    public void jpaEventBaseEntity() throws Exception {
+        //given
+        Member member = new Member("member1");
+        memberRepository.save(member); //@PrePersist
+        Thread.sleep(100);
+        member.setUsername("member2");
+        em.flush(); //@PreUpdate
+        em.clear();
+        //when
+        Member findMember = memberRepository.findById(member.getId()).get();
+        //then
+        System.out.println("findMember.createdDate = " + findMember.getCreatedDate());
+        System.out.println("findMember.updatedDate = " + findMember.getLastModifiedDate());
+        System.out.println("findMember.getCreatedBy = " + findMember.getCreatedBy());
+        System.out.println("findMember.getLastModifiedBy = " + findMember.getLastModifiedBy());
     }
 
 }
